3280d1ad9c4c558b5bb96cf52b0888dcb3fbeec8
unknown <RTI@2PLT-LAP-DCM-JM.SIRTI.ADDS>
2023-01-04 18:42:21 +0000

    Réalisation de la page de recherche de courrier

--- src/app/admin/search-mail/search-mail.component.ts
+++ src/app/admin/search-mail/search-mail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFileExport, faPencil, faPrint, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-mail',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-mail.component.scss']
 })
 export class SearchMailComponent {
+  faFileExport = faFileExport
+  faPrint = faPrint
+  faPencil = faPencil
+  faTrash = faTrash
 
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 5;
+  tableSizes: any = [5, 10, 15, 20];
+
+  data: any = [1, 2, 3, 4, 5, 6];
+
+  changeSize(value: string) {
+    this.tableSize = +value;
+  }
+
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.data;
+  }
+
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.data;
+  }
 }
