b0d98f4fa579d249e303ef6d80b6f865c09fb995
unknown <RTI@2PLT-LAP-DCM-JM.SIRTI.ADDS>
2023-01-12 16:42:59 +0000

    Réalisation du header du dashboard ainsi que de la partie statistiques par objet

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faFileExport, faMailBulk, faPause, faPrint, faUpRightFromSquare, faUserTie } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  faFileExport = faFileExport;
+  faPrint = faPrint;
+  faEnvelope = faEnvelope;
+  faMailBulk = faMailBulk;
+  faPause = faPause
+  faUpRightFromSquare = faUpRightFromSquare
+  faUserTie = faUserTie
 
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 5;
+  tableSizes: any = [5, 10, 15, 20];
+
+  data: any = [1, 2, 3, 4, 5, 6];
+
+  changeSize(value: string) {
+    this.tableSize = +value;
+  }
+
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.data;
+  }
+
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.data;
+  }
 }
