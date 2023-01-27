439007acb1a85a8db92bd7ea020dff05c7f242f2
unknown <RTI@2PLT-LAP-DCM-JM.SIRTI.ADDS>
2023-01-27 17:58:14 +0000

    Importation des services

--- src/app/admin/edit-mail/edit-mail.component.ts
+++ src/app/admin/edit-mail/edit-mail.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReceiverService } from 'src/app/services/receiver.service';
 
 @Component({
   selector: 'app-edit-mail',
   templateUrl: './edit-mail.component.html',
   styleUrls: ['./edit-mail.component.scss']
 })
-export class EditMailComponent {
+export class EditMailComponent implements OnInit {
+
+  services: any[] = []
+
+  constructor(
+    private receiverService: ReceiverService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllService();
+  }
+
+  getAllService() {
+    this.receiverService.getAllReceiver().subscribe(
+      response => {
+        this.services = response.results
+      }
+    );
+  }
+
   onBack() {
     history.back();
   }
